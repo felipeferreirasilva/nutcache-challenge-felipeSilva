3b61f8eb11adf4bec0df619e7ab9eb4f9b656e9f
Felipe Silva <felipeferreirasilva@gmail.com>
2022-05-25 19:37:34 -0300

    updates crudcrud token

--- src/api.js
+++ src/api.js
@@ -2,6 +2,6 @@ const URL = "https://crudcrud.com/api";
 const RESOURCE = "nutcache";
 
 // UPDATE WHEN CRUDCRUD EXPIRES
-const TOKEN = "53c0a4134def443a8b1fe861b1ca08b1";
+const TOKEN = "7f7416b607e04c13843569e90b97f641";
 
 export const API = `${URL}/${TOKEN}/${RESOURCE}`;
